af5629a0a00534ba7a16a245943209fe9e47599b
cgar246 <christiang3268@yahoo.com>
2021-09-23 21:34:48 -0700

    added pagetemplate

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
+const generatePage = require('./SRC/page-template');
+const fs = require('fs');
+
 const profileDataArgs = process.argv.slice(2);
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
 
-    console.log('================');
 
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
 
-    printProfileData(profileDataArgs);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
